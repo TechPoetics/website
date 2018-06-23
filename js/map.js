68cac46da68d0adfa86ac28669ce843ecd41315a
Alexander Lenail <alex@lenail.org>
2018-06-23 12:21:25 -0400

    [WIP] July 2018 (#8)
    July 2018

--- js/map.js
+++ js/map.js
@@ -1,36 +1,33 @@
-$(document).ready(function() {
-  var bittersMap = (function () {
-    var address = $('.address').text(),
-        geocoder = new google.maps.Geocoder(),
-        mapCanvas = document.getElementById('map-canvas'),
-        mapOptions = {
-          draggable: false,
-          disableDefaultUI: true,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          scrollwheel: false,
-          zoom: 15
-        },
-        map = new google.maps.Map(mapCanvas, mapOptions);
+function initMap() {
+
+    var address = $('.address').text();
+    var geocoder = new google.maps.Geocoder();
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+            draggable: false,
+            disableDefaultUI: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: false,
+            zoom: 15
+        };
+
+    var map = new google.maps.Map(mapCanvas, mapOptions);
 
     geocoder.geocode({ 'address': address }, function(results, status) {
-      var latLon = results[0].geometry.location;
-      map.setCenter(latLon);
+        var latLon = results[0].geometry.location;
+        map.setCenter(latLon);
 
-      marker = new google.maps.Marker({ map: map, position: latLon });
+        marker = new google.maps.Marker({ map: map, position: latLon });
     });
 
-    return {
-      init: function () {
-        map.set('styles', [{
-          featureType: 'landscape',
-          elementType: 'geometry',
-          stylers: [
+    map.set('styles', [{
+        featureType: 'landscape',
+        elementType: 'geometry',
+        stylers: [
             { hue: '#ffff00' },
             { saturation: 30 },
             { lightness: 10}
-          ]}
-        ]);
-      }
-    };
-  }());
-});
+        ]}
+    ]);
+
+};
