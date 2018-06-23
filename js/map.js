1d40e2c051e5355aa663df25e47dfa9dc5e37ad9
Alexander Lenail <zfrenchee@gmail.com>
2018-06-23 11:21:25 -0400

    Prepare for july

--- js/map.js
+++ js/map.js
@@ -1,36 +1,36 @@
-$(document).ready(function() {
-  var bittersMap = (function () {
-    var address = $('.address').text(),
-        geocoder = new google.maps.Geocoder(),
-        mapCanvas = document.getElementById('map-canvas'),
-        mapOptions = {
-          draggable: false,
-          disableDefaultUI: true,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          scrollwheel: false,
-          zoom: 15
-        },
-        map = new google.maps.Map(mapCanvas, mapOptions);
+// $(document).ready(function() {
+//   var bittersMap = (function () {
+//     var address = $('.address').text(),
+//         geocoder = new google.maps.Geocoder(),
+//         mapCanvas = document.getElementById('map-canvas'),
+//         mapOptions = {
+//           draggable: false,
+//           disableDefaultUI: true,
+//           mapTypeId: google.maps.MapTypeId.ROADMAP,
+//           scrollwheel: false,
+//           zoom: 15
+//         },
+//         map = new google.maps.Map(mapCanvas, mapOptions);
 
-    geocoder.geocode({ 'address': address }, function(results, status) {
-      var latLon = results[0].geometry.location;
-      map.setCenter(latLon);
+//     geocoder.geocode({ 'address': address }, function(results, status) {
+//       var latLon = results[0].geometry.location;
+//       map.setCenter(latLon);
 
-      marker = new google.maps.Marker({ map: map, position: latLon });
-    });
+//       marker = new google.maps.Marker({ map: map, position: latLon });
+//     });
 
-    return {
-      init: function () {
-        map.set('styles', [{
-          featureType: 'landscape',
-          elementType: 'geometry',
-          stylers: [
-            { hue: '#ffff00' },
-            { saturation: 30 },
-            { lightness: 10}
-          ]}
-        ]);
-      }
-    };
-  }());
-});
+//     return {
+//       init: function () {
+//         map.set('styles', [{
+//           featureType: 'landscape',
+//           elementType: 'geometry',
+//           stylers: [
+//             { hue: '#ffff00' },
+//             { saturation: 30 },
+//             { lightness: 10}
+//           ]}
+//         ]);
+//       }
+//     };
+//   }());
+// });
